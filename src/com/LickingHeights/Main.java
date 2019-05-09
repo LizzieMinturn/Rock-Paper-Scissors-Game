e036f038b4b1daaf6ea8a7983acc8c52d336e9c2
lizzie <elizabeth.minturn.2021@student.lhschools.org>
2019-05-09 07:44:33 -0400

    started game

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,16 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        boolean value=true;
+        while(value){
+            System.out.println("rock...\npaper...\nscissors...");
+            System.out.println("choose!");
+        }
+    }
+}
