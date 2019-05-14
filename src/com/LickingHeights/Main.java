c3883fb484d51b0cfcc5489a250512cbf15114c7
lizzie <elizabeth.minturn.2021@student.lhschools.org>
2019-05-14 07:47:50 -0400

    linked the player choice to my array

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //using if/else statement, or switches to build the core game
         //rock beats scissors, scissors beats paper, paper beats rock
 
         Scanner keyboard = new Scanner(System.in);
@@ -16,12 +15,12 @@ public class Main {
         //loop for game to start completely over
         while(value){
         System.out.println("Let's play a game of rock, paper, scissors!");
-        System.out.println("what's your first move?");
+        //score keeping veriable
         int playerWins = 0;
         int computerWins = 0;
         //loop for core game code
         while (value) {
-            System.out.println("what's your next move?");
+            System.out.println("what's your move?");
             String Player = keyboard.nextLine();
 
             //use simple array to pull computer choice
@@ -30,8 +29,18 @@ public class Main {
             choose[1] = "paper";
             choose[2] = "scissors";
             int Computer = new Random().nextInt(choose.length);
+                    //for player choice
+                    if(Player=="rock"){
+                        Player=choose[0];
+                    }
+                    else if(Player=="paper"){
+                        Player=choose[1];
+                    }
+                    else if(Player=="scissors"){
+                        Player=choose[2];
+                    }
 
-                    //for computer wins
+                    //for computer choice
                     if(Computer==0){
                         System.out.println("I choose rock");
                     }
@@ -42,9 +51,33 @@ public class Main {
                         System.out.println("I choose scissors");
                     }
 
-                    //score keeping veriable with feedback
-                    if(){
-
+                    //feedback
+                    //using if/else statement, or switches to build the core game
+                    //player loses
+                    if(Computer==0&&Player==choose[2]){
+                        System.out.println("Computer Score: "+ computerWins+1);
+                        System.out.println("Player Score: "+ playerWins+0);
+                    }
+                    else if(Computer==1&&Player==choose[0]){
+                        System.out.println("Computer Score: "+ computerWins+1);
+                        System.out.println("Player Score: "+ playerWins+0);
+                    }
+                    else if(Computer==2&&Player==choose[1]) {
+                        System.out.println("Computer Score: " + computerWins+1);
+                        System.out.println("Player Score: " + playerWins+0);
+                    }
+                    //player wins
+                    else if(Computer==0&&Player==choose[1]) {
+                        System.out.println("Computer Score: " + computerWins+0);
+                        System.out.println("Player Score: " + playerWins+1);
+                    }
+                    else if(Computer==1&&Player==choose[2]) {
+                        System.out.println("Computer Score: " + computerWins+0);
+                        System.out.println("Player Score: " + playerWins+1);
+                    }
+                    else if(Computer==2&&Player==choose[0]) {
+                        System.out.println("Computer Score: " + computerWins+0);
+                        System.out.println("Player Score: " + playerWins+1);
                     }
                     }
                 }
