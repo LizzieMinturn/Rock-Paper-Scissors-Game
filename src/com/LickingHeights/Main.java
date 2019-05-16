1741dc265b3e2eb15f81d6fb5cd8e780c4ef3c5c
lizzie <elizabeth.minturn.2021@student.lhschools.org>
2019-05-16 07:36:08 -0400

    created method for when computer beats user

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -13,73 +13,53 @@ public class Main {
 
         boolean value = true;
         //loop for game to start completely over
-        while(value){
-        System.out.println("Let's play a game of rock, paper, scissors!");
-        //score keeping veriable
-        int playerWins = 0;
-        int computerWins = 0;
-        //loop for core game code
         while (value) {
-            System.out.println("what's your move?");
-            String Player = keyboard.nextLine();
+            System.out.println("Let's play a game of rock, paper, scissors!");
+            //score keeping veriable
+            int playerScore = 0;
+            int computerScore = 0;
+            //loop for core game code
+            while (value) {
+                System.out.println("what's your move?");
+                String Player = keyboard.nextLine();
 
-            //use simple array to pull computer choice
-            String choose[] = new String[3];
-            choose[0] = "rock";
-            choose[1] = "paper";
-            choose[2] = "scissors";
-            int Computer = new Random().nextInt(choose.length);
-                    //for player choice
-                    if(Player=="rock"){
-                        Player=choose[0];
-                    }
-                    else if(Player=="paper"){
-                        Player=choose[1];
-                    }
-                    else if(Player=="scissors"){
-                        Player=choose[2];
-                    }
+                //use simple array to pull computer choice
+                String choose[] = new String[3];
+                choose[0] = "rock";
+                choose[1] = "paper";
+                choose[2] = "scissors";
+                int Computer = new Random().nextInt(choose.length);
 
-                    //for computer choice
-                    if(Computer==0){
-                        System.out.println("I choose rock");
-                    }
-                    else if(Computer==1){
-                        System.out.println("I choose paper");
-                    }
-                    else if(Computer==2){
-                        System.out.println("I choose scissors");
-                    }
+                //using if/else statement, or switches to build the core game
+                //for player choice
 
-                    //feedback
-                    //using if/else statement, or switches to build the core game
-                    //player loses
-                    if(Computer==0&&Player==choose[2]){
-                        System.out.println("Computer Score: "+ computerWins+1);
-                        System.out.println("Player Score: "+ playerWins+0);
-                    }
-                    else if(Computer==1&&Player==choose[0]){
-                        System.out.println("Computer Score: "+ computerWins+1);
-                        System.out.println("Player Score: "+ playerWins+0);
-                    }
-                    else if(Computer==2&&Player==choose[1]) {
-                        System.out.println("Computer Score: " + computerWins+1);
-                        System.out.println("Player Score: " + playerWins+0);
-                    }
-                    //player wins
-                    else if(Computer==0&&Player==choose[1]) {
-                        System.out.println("Computer Score: " + computerWins+0);
-                        System.out.println("Player Score: " + playerWins+1);
-                    }
-                    else if(Computer==1&&Player==choose[2]) {
-                        System.out.println("Computer Score: " + computerWins+0);
-                        System.out.println("Player Score: " + playerWins+1);
-                    }
-                    else if(Computer==2&&Player==choose[0]) {
-                        System.out.println("Computer Score: " + computerWins+0);
-                        System.out.println("Player Score: " + playerWins+1);
-                    }
-                    }
+                //for computer choice
+                if (Computer == 0) {
+                    System.out.println("I choose rock");
+                } else if (Computer == 1) {
+                    System.out.println("I choose paper");
+                } else if (Computer == 2) {
+                    System.out.println("I choose scissors");
                 }
+                //player wins
+
             }
         }
+
+    }   //feedback
+        //player loses
+        public static int ComputerWins(String Player, int Computer, int ComputerScore){
+            if(Player=="rock"&&Computer==1 ||
+                    Player=="paper"&&Computer==2 ||
+                    Player=="scissors"&&Computer==0)
+                ComputerScore=ComputerScore+1;
+            return ComputerScore;
+        }
+        public static int PlayerWins(String Player, int Computer, int PlayerScore){
+            if(Player=="rock"&&Computer==1 ||
+                    Player=="paper"&&Computer==2 ||
+                    Player=="scissors"&&Computer==0)
+                PlayerScore=PlayerScore+1;
+            return PlayerScore;
+
+}
