58a6e30c4abf5776e7f8ef7e435db2e49f17b8ac
lizzie <elizabeth.minturn.2021@student.lhschools.org>
2019-05-16 08:36:42 -0400

    worked on score feedback and the loop for the game to start over

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -7,17 +7,14 @@ public class Main {
 
     public static void main(String[] args) {
         //rock beats scissors, scissors beats paper, paper beats rock
-
         Scanner keyboard = new Scanner(System.in);
 
-
         boolean value = true;
+        boolean win=true;
         //loop for game to start completely over
         while (value) {
             System.out.println("Let's play a game of rock, paper, scissors!");
             //score keeping veriable
-            int PlayerScore=0;
-            int ComputerScore=0;
             //loop for core game code
             while (value) {
                 System.out.println("what's your move?");
@@ -31,8 +28,6 @@ public class Main {
                 int Computer = new Random().nextInt(choose.length);
 
                 //using if/else statement, or switches to build the core game
-                //for player choice
-
                 //for computer choice
                 if (Computer == 0) {
                     System.out.println("I choose rock");
@@ -41,27 +36,40 @@ public class Main {
                 } else if (Computer == 2) {
                     System.out.println("I choose scissors");
                 }
-                System.out.println("Computer Score: "+ComputerWins(Player, Computer, ComputerScore));
-                System.out.println("Player Score: "+PlayerWins(Player, Computer, PlayerScore));
+
+                int ComputerScore=ComputerWins(Player, Computer, ComputerScore);
+                int PlayerScore=PlayerWins(Player, Computer, PlayerScore);
+                System.out.println("Computer Score: "+ComputerScore);
+                System.out.println("Player Score: "+PlayerScore);
             }
         }
-    }   //feedback
-
+    }
+    //feedback
     //player loses
     public static int ComputerWins(String Player, int Computer, int ComputerScore) {
         if (Player == "rock" && Computer == 1 ||
                 Player == "paper" && Computer == 2 ||
                 Player == "scissors" && Computer == 0)
-            ComputerScore = ComputerScore++;
+            ComputerScore = ComputerScore+1;
+        else
+            ComputerScore=ComputerScore+0;
         return ComputerScore;
     }
-
     //player wins
     public static int PlayerWins(String Player, int Computer, int PlayerScore) {
         if (Player == "scissors" && Computer == 1 ||
                 Player == "rock" && Computer == 2 ||
                 Player == "paper" && Computer == 0)
-            PlayerScore = PlayerScore + 1;
+            PlayerScore = PlayerScore+1;
+        else
+            PlayerScore=PlayerScore+0;
         return PlayerScore;
+    }
+    public static boolean StartOver(boolean win, int PlayerScore, int ComputerScore){
+        if(PlayerScore==2||ComputerScore==2){
+            win=true;
+        }
+        else win=false;
+        return win;
     }
 }
